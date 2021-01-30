08fd343fb928b5682503ade174e96de1be263302
dionata.silva <dionata.silva@cwi.com.br>
2021-01-30 18:38:42 -0300

    Adicionando styled component

--- src/pages/components/main/index.tsx
+++ src/pages/components/main/index.tsx
@@ -1,9 +1,19 @@
-const Main = () => {
-  return (
-    <main>
-      <h1>meu primeiro teste!</h1>
-    </main>
-  )
-}
+import * as S from './styles'
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/hero-illustration.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
+)
 
 export default Main
--- src/pages/components/main/test.tsx
+++ src/pages/components/main/test.tsx
@@ -6,9 +6,15 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /meu primeiro teste!/i })
+      screen.getByRole('heading', { name: /React Avançado/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
