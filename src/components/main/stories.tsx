2487c09a9b9bd14d981b2d84d96629e4e1a81157
dionata.silva <dionata.silva@cwi.com.br>
2021-01-31 22:13:50 -0300

    Adicionando storybook

---
+++ src/components/main/stories.tsx
@@ -0,0 +1,13 @@
+import Main from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'teste',
+  description: 'test'
+}
